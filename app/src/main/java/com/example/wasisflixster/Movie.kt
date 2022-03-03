4b5a63cbd481ecb681c8f0aaf57b1ab2e7c6f52d
Wasis Ali <42586390+WA64@users.noreply.github.com>
2022-03-03 00:53:41 -0500

    Added DetailView WIP

--- app/src/main/java/com/example/wasisflixster/Movie.kt
+++ app/src/main/java/com/example/wasisflixster/Movie.kt
@@ -1,17 +1,26 @@
 package com.example.wasisflixster
 
+import android.os.Parcelable
 import com.codepath.asynchttpclient.AsyncHttpClient
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
+
 import org.json.JSONArray
 
+@Parcelize
 class Movie (
     val movieId: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overView: String,
     private val backdropPath: String,
 
-) {
+) : Parcelable
+{
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    @IgnoredOnParcel
     val backdropImageUrl = "https://image.tmdb.org/t/p/w342/$backdropPath"
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
@@ -24,6 +33,7 @@ class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average" ),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview"),
